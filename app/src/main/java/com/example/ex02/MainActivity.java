9f969897cfe132e91d64f320e6ab4fbb8b132ad8
Yasser <yassermechhab007@gmail.com>
2023-01-22 17:41:56 +0100

    bugs fixed

--- app/src/main/java/com/example/ex02/MainActivity.java
+++ app/src/main/java/com/example/ex02/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button quitter_btn,reinitialiser_btn;
     EditText nbr_field;
+    TextView multiplication_table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
         reinitialiser_btn =findViewById(R.id.reinitialiser_btn);
         nbr_field=findViewById(R.id.nbr_field);
+        multiplication_table=findViewById(R.id.multiplication_table);
         reinitialiser_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 nbr_field.setText("");
+                String etat0="? * 0 = ?\n? * 1 = ?\n? * 2 = ?\n? * 3 = ?\n? * 4 = ?\n? * 4 = ?\n? * 5 = ?\n? * 6 = ?\n? * 7 = ?\n? * 8 = ?\n? * 9 = ?\n? * 10 = ?";
+                multiplication_table.setText(etat0);
             }
         });
     }
