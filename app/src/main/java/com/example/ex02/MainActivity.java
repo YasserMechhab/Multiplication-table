55951815e593e2f722112e2a0b0b5ce7073e7164
Yasser <yassermechhab007@gmail.com>
2023-01-22 16:28:34 +0100

    MainActivity.java : give both Button(quitter/reinitialiser) role

--- app/src/main/java/com/example/ex02/MainActivity.java
+++ app/src/main/java/com/example/ex02/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.ex02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button quitter_btn,reinitialiser_btn;
+    EditText nbr_field;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quitter_btn = findViewById(R.id.quitter_btn);
+        quitter_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
+        reinitialiser_btn =findViewById(R.id.reinitialiser_btn);
+        nbr_field=findViewById(R.id.nbr_field);
+        reinitialiser_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nbr_field.setText("");
+            }
+        });
     }
 }
